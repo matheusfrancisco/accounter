59feeb09a572571929bc5e8e6cba4482943cd966
matheusfrancisco <matheusmachadoufsc@gmail.com>
2019-12-12 13:14:10 -0300

    Edit holderController

--- src/view/holder-controller.ts
+++ src/view/holder-controller.ts
@@ -1,12 +1,19 @@
 interface HolderService {
-    createHolder: (name: string, taxpayerRegister: string) => void;
+    createHolder: (name: string, taxpayerRegistry: string) => Promise<void>;
 }
 
 const buildHolderController = (holderService: HolderService) => ({
-    postHolder: (req: any, res: any) => {
-        holderService.createHolder(req.body.name, req.body.taxpayerRegister);
-        console.log(res)
-        res.status(201).end();
+    postHolder: async (req: any, res: any) => {
+        if (!req.body.name || !req.body.taxpayerRegistry) {
+            res.status(400).end();
+            return;
+        }
+        try {
+            await holderService.createHolder(req.body.name, req.body.taxpayerRegistry);            
+            res.status(201).end();
+        } catch (error) {
+            res.status(400).end();
+        }
     },
 });
 
