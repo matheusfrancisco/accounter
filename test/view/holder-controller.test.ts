5abbae93149b3937a62f638150e81c73dd447abb
matheusfrancisco <matheusmachadoufsc@gmail.com>
2019-12-12 13:13:57 -0300

    Add test to holderController

--- test/view/holder-controller.test.ts
+++ test/view/holder-controller.test.ts
@@ -7,31 +7,61 @@ const expect = chai.expect;
 import { buildHolderController } from '../../src/view/holder-controller';
 
 describe('HolderController', () => {
-    it('Should receive name and taxpayeresgiter and delegate to application layer', () => {
-        const createHolder = sinon.spy();
-        const holderServiceMock = { createHolder };
-        const holderName = 'Matheus';
-        const holderTaxpayerResgiter = '30330322210';
-        const req = {body: {name: holderName , taxpayerRegister: holderTaxpayerResgiter}}
-        const holderController = buildHolderController(holderServiceMock);
-        holderController.postHolder(req, {});
-        expect(createHolder).to.have.been
-            .calledWith(holderName, holderTaxpayerResgiter);
+    let createHolder: sinon.SinonSpy;
+    let holderServiceMock: { createHolder: sinon.SinonSpy };
+    let holderController: any;
+    let failingHolderController: any;
+    let end: any;
+    let endMock: any;
+    let status: any;
+    let resMock: any;
+    
+    const holderName = 'Matheus';
+    const holderTaxpayerRegistry = '30330322210';
+    const req = {body: {name: holderName , taxpayerRegistry: holderTaxpayerRegistry}};
+    const reqWithoutName = {body: { taxpayerRegistry: holderTaxpayerRegistry}};
+    const reqWithoutTaxpayerRegistry = {body: { name: holderName }};
+    
+    //TODO 
+    // Todos os postHolder com await antes.
+    // O createHolder vai ter que retornar uma Promise<sinon> (sinon spy promise)
+    beforeEach(() => {
+        createHolder = sinon.spy();
+        holderServiceMock = { createHolder };
+        holderController = buildHolderController(holderServiceMock);
+        failingHolderController = buildHolderController({ createHolder: () => { throw new Error(); }});
+        end = sinon.spy();
+        endMock = { end };
+        status = sinon.spy(() => endMock);
+        resMock = { status };
     });
-    it('Should return status code 201', () => {
-        const createHolder = sinon.spy();
-        const holderServiceMock = { createHolder };
-        const end = sinon.spy();
-        const endMock = { end };
-        const status = sinon.spy(() => endMock);
-        const resMock = { status };
-        const holderName = 'Matheus';
-        const holderTaxpayerResgiter = '30330322210';
-        const req = {body: {name: holderName , taxpayerRegister: holderTaxpayerResgiter}}
-        const holderController = buildHolderController(holderServiceMock);
+    
+    it('Should receive name and taxpayeresgiter and delegate to application layer', () => {
         holderController.postHolder(req, resMock);
-        expect(status).to.have.been
-            .calledWith(201);
+        expect(createHolder).to.have.been.calledWith(holderName, holderTaxpayerRegistry);
+    });
+
+    it('Should return status code 201', () => {
+        holderController.postHolder(req, resMock);
+        expect(status).to.have.been.calledWith(201);
         expect(end).to.have.been.called;
     }); 
+
+    it('Should return status code 400 if name is not present', () => {
+        holderController.postHolder(reqWithoutName, resMock);
+        expect(status).to.have.been.calledWith(400);
+        expect(end).to.have.been.called;
+    });
+
+    it('Should return status code 400 if taxpayerregistry is not present', () => {
+        holderController.postHolder(reqWithoutTaxpayerRegistry, resMock);
+        expect(status).to.have.been.calledWith(400);
+        expect(end).to.have.been.called;
+    });
+
+    it('Should return status code 400 if error thrown', () => {
+        failingHolderController.postHolder(req, resMock);
+        expect(status).to.have.been.calledWith(400);
+        expect(end).to.have.been.called;
+    });
 })
