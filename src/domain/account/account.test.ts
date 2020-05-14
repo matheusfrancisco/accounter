de1ded0d940b922795d55fb9553288295a01135c
Lucas Miranda <lucas.miranda@gupy.com.br>
2020-05-14 12:34:48 -0300

    Implement multi holders support on account
    Co-authored-by: rebecamorais <rebecamorais.12@gmail.com>
    Co-authored-by: Matheus Francisco <matheusmachadoufsc@gmail.com>

--- src/domain/account/account.test.ts
+++ src/domain/account/account.test.ts
@@ -1,5 +1,6 @@
 import Holder from '../holder/holder';
-import Account from './account';
+import Account, { errorMessages } from './account';
+
 import TaxpayerRegistry from '../holder/taxpayer-registry';
 import { countries } from '../holder/country';
 
@@ -8,21 +9,31 @@ describe('Account', () => {
     it('require a holder to be provided', () => {
       const taxpayerRegistry = new TaxpayerRegistry('13464210642', countries.BR);
       const holder = new Holder('Lucas', taxpayerRegistry);
-      const account = new Account(holder);
+      const account = new Account([holder]);
 
       expect(account.holders).toEqual([holder]);
     });
-  });
 
-  describe('addHolder', () => {
-    it('Adds a new account holder', () => {
+    it('can recieve more than one holder', () => {
       const taxpayerRegistry = new TaxpayerRegistry('13464210642', countries.BR);
       const lucas = new Holder('Lucas', taxpayerRegistry);
       const matheus = new Holder('Matheus', taxpayerRegistry);
-      const account = new Account(lucas);
+      const account = new Account([lucas, matheus]);
 
-      account.addHolder(matheus);
       expect(account.holders).toEqual([lucas, matheus]);
+    });
+
+    it('should throw exception when holders is empty', () => {
+      expect(() => new Account([])).toThrow(errorMessages.zeroHolders);
+    });
+
+    it('should throw exception when more than 5 holders are provided', () => {
+      const taxpayerRegistry = new TaxpayerRegistry('13464210642', countries.BR);
+      const holder = new Holder('Matheus', taxpayerRegistry);
+
+      expect(() => new Account([holder, holder, holder, holder, holder, holder])).toThrow(
+        errorMessages.maxHoldersExceeded,
+      );
     });
   });
 });
--- src/domain/account/account.ts
+++ src/domain/account/account.ts
@@ -1,14 +1,21 @@
 import Holder from '../holder/holder';
+import { ServiceError } from '../../service-error';
+
+const MAX_HOLDER_COUNT = 5;
+
+export const errorMessages = {
+  zeroHolders: 'An account must have at least one Holder',
+  maxHoldersExceeded: `Account must have no more than ${MAX_HOLDER_COUNT} holders`,
+};
 
 export default class Account {
-  readonly holders: Holder[] = [];
+  readonly holders: Holder[];
 
-  constructor(readonly holder: Holder) {
-    this.holders.push(holder);
-  }
+  constructor(holders: Holder[]) {
+    if (!holders.length) throw new ServiceError(errorMessages.zeroHolders);
+    if (holders.length > MAX_HOLDER_COUNT) throw new ServiceError(errorMessages.maxHoldersExceeded);
 
-  addHolder(holder: Holder) {
-    this.holders.push(holder);
+    this.holders = holders;
   }
 
   // TODO: Alterar construtor para receber os holders
